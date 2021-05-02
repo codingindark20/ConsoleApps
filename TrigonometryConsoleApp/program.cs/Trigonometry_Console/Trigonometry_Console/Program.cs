250caca933b3e89ccad4dc28085f72984de4dbf7
Codingindark <rochakrupakheti20@gmail.com>
2021-05-03 00:02:46 +0545

    Console app TrigonometryConsoleApp created

---
+++ TrigonometryConsoleApp/program.cs/Trigonometry_Console/Trigonometry_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Trigonometry_Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ///Declaration of variables
+            float angle = 60;
+
+            ///Sine of angle
+            float sine = (float)Math.Sin(angle * (float)Math.PI / 180);
+
+            ///Cosine of angle
+            float cosine = (float)Math.Cos(angle * (float)Math.PI / 180);
+
+            ///Output
+            Console.WriteLine("Sine of " +angle + "is : " +sine);
+            Console.WriteLine("Cosine of " +angle + "is : " +cosine);
+        }
+    }
+}
